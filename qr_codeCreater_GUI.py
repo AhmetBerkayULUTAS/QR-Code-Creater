52f80f0d6dde82be287d169d91c3ca14bccc5dbc
Ahmet Berkay ULUTAŞ <75786865+AhmetBerkayULUTAS@users.noreply.github.com>
2024-02-23 00:56:06 +0300

    fixed some bug and recoded clean

--- qr_codeCreater_GUI.py
+++ qr_codeCreater_GUI.py
@@ -2,6 +2,7 @@ import qrcode
 from tkinter import * 
 from tkinter import filedialog
 from PIL import Image,ImageTk
+import os
 
 root = Tk()
 root.title("QR code Genetator")
@@ -14,26 +15,26 @@ img_path = ""
 def select_image():
     global img_path
     img_path = filedialog.askopenfilename(title="Select an image file",filetypes=[("Image files", "*.jpg *.jpeg *.png")])
-    
-def saved_path(name):
-    qr_saved_path="./qrcodeCreater_project/qrcodes/"+name+".png"
-    return qr_saved_path
 
+    
 global name
 name=""
-def generate():
-    global img_path, name
+def saved_path(name):
+    qr_saved_path="./qrcodes/"+name+".png"
+    return qr_saved_path
 
-    name = str(TitleEntry.get()) 
-    text = LinkEntry.get()
-    qr=qrcode.make(text)
 
-    if img_path:
-        img=Image.open(img_path).convert("L")
-        qr_size = qr.size[0]
-        img.thumbnail((qr_size/4, qr_size/4))
-        img_pos = ((qr_size-img.size[0]) // 2,(qr_size - img.size[1]) // 2)
-        qr.paste(img,img_pos)
+def img_center_of_qr(img_path,qr):
+    img=Image.open(img_path).convert("L")
+    qr_size = qr.size[0]
+    img.thumbnail((qr_size/4, qr_size/4))
+    img_pos = ((qr_size-img.size[0]) // 2,(qr_size - img.size[1]) // 2)
+    qr.paste(img,img_pos)
+
+
+def save_and_show(qr,name):
+    if not os.path.exists("./qrcodes"):
+        os.makedirs("./qrcodes")
 
     qr.save(saved_path(name))
     
@@ -41,7 +42,24 @@ def generate():
     ImageLabel.config(image=qr_Image)
     ImageLabel.image = qr_Image
 
+    
+def generate():
+    global img_path, name
+
+    name = str(TitleEntry.get()) 
+    text = LinkEntry.get()
+    qr=qrcode.make(text)
+        
+    if qr.size[0]>450:
+       qr = qr.resize((450, 450))
+
+    if img_path:
+        img_center_of_qr(img_path,qr)
+
+    save_and_show(qr,name)
+    qr_color_changer(TRUE)
     img_path = ""
+
 
 def qr_color_changer(_):
 
@@ -60,29 +78,27 @@ def qr_color_changer(_):
             if pixelr != 255 and pixelg != 255 and pixelb != 255: 
                 img.putpixel((x, y), (r, g, b))
     
-    img.save(saved_path(name))
+    save_and_show(img,name)
     
-    qr_Image=ImageTk.PhotoImage(file=saved_path(name))
-    ImageLabel.config(image=qr_Image)
-    ImageLabel.image = qr_Image
-    
+
 def toggle_button():
     if check_var.get():
         Select_img_Button.config(state=NORMAL)  
     else:
         Select_img_Button.config(state=DISABLED) 
 
+
 ImageLabel = Label(root,bg="#FFB266")
 ImageLabel.pack(padx=20,pady=10,side=RIGHT)
 
 slider_r = Scale(root, from_=0, to=250, orient="horizontal", label="QR Kırmızı", command=qr_color_changer)
-slider_r.place(x=450, y=20)
+slider_r.place(x=420, y=20)
 
 slider_g = Scale(root, from_=0, to=250, orient="horizontal", label="QR Yeşil", command=qr_color_changer)
-slider_g.place(x=450, y=75)
+slider_g.place(x=420, y=75)
 
 slider_b = Scale(root, from_=0, to=250, orient="horizontal", label="QR Mavi", command=qr_color_changer)
-slider_b.place(x=450, y=130)
+slider_b.place(x=420, y=130)
 
 TitleLabel = Label(root,text="Title",bg="#FFB266",font="ariel 15 bold")
 TitleLabel.place(x=50,y=180)
